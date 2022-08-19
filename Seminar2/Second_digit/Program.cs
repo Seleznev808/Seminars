78115c41b7dce55407f8079bb9976c79140db899
Seleznev808 <109744835+Seleznev808@users.noreply.github.com>
2022-08-19 20:33:22 +0300

    Add files via upload

---
+++ Seminar2/Second_digit/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+if (number.Length != 3)
+{
+    Console.WriteLine("Попробуйте снова!");
+}
+else
+{
+    Console.WriteLine("Вторая цифра вашего числа - " + number[1]);
+}
