86937c25daeb77fc07816d6e5676d0445611d713
Seleznev808 <109744835+Seleznev808@users.noreply.github.com>
2022-08-19 20:32:33 +0300

    Add files via upload

---
+++ Seminar2/Day_of_week/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int number = int.Parse(Console.ReadLine());
+if (number > 7 | number < 1)
+{
+    Console.WriteLine("Попробуйте снова!");
+}
+else if (number == 6 | number == 7)
+{
+    Console.WriteLine("Этот день выходной!");
+}
+else
+{
+    Console.WriteLine("Этот день будний");
+}
