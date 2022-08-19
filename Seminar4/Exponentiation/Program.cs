72f63d101c706ce38af6f0143cc4d82b945ccbb2
Seleznev808 <seleznev.2016@yandex.ru>
2022-08-19 21:05:24 +0300

    add exponentiation

---
+++ Seminar4/Exponentiation/Program.cs
@@ -0,0 +1,24 @@
+﻿int Exponentiation (int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число А");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число В");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberB < 0)
+{
+    Console.WriteLine("Попробуйте еще раз!");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine(numberA + " ^ "+ numberB + " = " + Exponentiation (numberA, numberB));
+}
