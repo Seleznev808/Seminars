3ecd99942d3df0f581376c2a8fc5faf5080c0866
Seleznev808 <109744835+Seleznev808@users.noreply.github.com>
2022-08-19 20:37:17 +0300

    Add files via upload

---
+++ Seminar3/Distance_points/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите координаты точки А");
+double xa = double.Parse(Console.ReadLine());
+double ya = double.Parse(Console.ReadLine());
+double za = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В");
+double xb = double.Parse(Console.ReadLine());
+double yb = double.Parse(Console.ReadLine());
+double zb = double.Parse(Console.ReadLine());
+
+double distancePoint = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
+Console.WriteLine("Расстояние между точками А и В - " + Math.Round(distancePoint, 2));
