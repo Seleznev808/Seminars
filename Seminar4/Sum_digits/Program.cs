def6effd05548226fc862225cadba600d916062d
Seleznev808 <seleznev.2016@yandex.ru>
2022-08-19 22:51:49 +0300

    add sum digits

---
+++ Seminar4/Sum_digits/Program.cs
@@ -0,0 +1,13 @@
+﻿double SumDigits (string number)
+{
+    double sum = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        sum += Char.GetNumericValue(number[i]);
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+Console.WriteLine("Сумма цифр в числе " + number + " - " + SumDigits(number));
