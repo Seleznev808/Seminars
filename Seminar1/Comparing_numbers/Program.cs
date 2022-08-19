636d13334e00b4db5d7d0ab82b296e9a79181c19
Seleznev808 <109744835+Seleznev808@users.noreply.github.com>
2022-08-19 20:28:28 +0300

    Add files via upload

---
+++ Seminar1/Comparing_numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿// Только целые числа
+
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Число " + numberA + " больше " + numberB);
+}
+
+if (numberA < numberB)
+{
+    Console.WriteLine("Число " + numberB + " больше " + numberA);
+}
+
+if (numberA == numberB)
+{
+     Console.WriteLine("Число " + numberA + " равно " + numberB);
+}
