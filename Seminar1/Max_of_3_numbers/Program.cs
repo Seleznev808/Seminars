6286fea97e7babef07e6fdce856198f5553143bc
Seleznev808 <109744835+Seleznev808@users.noreply.github.com>
2022-08-19 20:31:15 +0300

    Add files via upload

---
+++ Seminar1/Max_of_3_numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int numberD = int.Parse(Console.ReadLine());
+
+int maxNumber = numberA;
+
+if (numberB > maxNumber)
+{
+    maxNumber = numberB;
+}
+
+if (numberD > maxNumber)
+{
+    maxNumber = numberD;
+}
+
+Console.WriteLine("Максимальное из этих трех чисел - " + maxNumber);
