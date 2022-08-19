474d5e71502875fc48fc0cb8dd4bda2981c12495
Seleznev808 <109744835+Seleznev808@users.noreply.github.com>
2022-08-19 20:36:10 +0300

    Add files via upload

---
+++ Seminar3/Cubes/Program.cs
@@ -0,0 +1,20 @@
+﻿void Cubes(int numberN)
+{
+    for (int i = 1; i <= numberN; i++)
+    {
+        double cube = Math.Pow(i, 3);
+        Console.Write(cube + " ");
+    }
+}
+
+Console.WriteLine("Введите число N (большее или равное 1)");
+int N = int.Parse(Console.ReadLine());
+if (N >= 1)
+{
+    Console.WriteLine("Таблица кубов от 1 до " + N + ":");
+    Cubes(N);
+}
+else
+{
+    Console.WriteLine("Попробуйте еще раз!");
+}
