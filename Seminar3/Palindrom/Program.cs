117908676993b2bcda2d47bae33f1d89ad4b7f24
Seleznev808 <109744835+Seleznev808@users.noreply.github.com>
2022-08-19 20:38:22 +0300

    Add files via upload

---
+++ Seminar3/Palindrom/Program.cs
@@ -0,0 +1,15 @@
+﻿void Palindrom(string str)
+{
+    if (str[0] == str[4] && str[1] == str[3])
+    {
+        Console.WriteLine("Число " + str + " - палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число " + str + " - не является палиндромом");
+    }
+}
+
+Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+Palindrom(number);
